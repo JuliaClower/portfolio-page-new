b28d6d0d81dd15594ff4b64d9e76485409778e4a
Julia Clower <juliaclower@Julias-MacBook-Air.local>
2019-12-11 15:13:06 -0500

    updated image path location

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,18 +11,18 @@ export default class Footer extends Component {
                         <h1></h1>
                         <div className="contact-top-row">
                             <a href="mailto:juliavanclower@gmail.com" target="_blank">
-                                <img alt="email icon" src="/images/circle-email-icon.png"/> <h3>JuliaVanClower@gmail.com</h3>
+                                <img alt="email icon" src="/media/circle-email-icon.png"/> <h3>JuliaVanClower@gmail.com</h3>
                             </a>
                             <a href="https://github.com/JuliaClower" target="blank">
-                                <img alt="github icon" src="/images/github-icon.png" /> <h3>github.com/JuliaClower</h3>
+                                <img alt="github icon" src="/media/github-icon.png" /> <h3>github.com/JuliaClower</h3>
                             </a>
                         </div>
                         <div className="contact-bottom-row">
                             <a href="https://www.linkedin.com/in/juliavanclower/" target="blank">
-                                <img alt="linkedin icon" src="/images/linkedin-icon.png" /> <h3>linkedin.com/in/juliavanclower/ </h3>
+                                <img alt="linkedin icon" src="/media/linkedin-icon.png" /> <h3>linkedin.com/in/juliavanclower/ </h3>
                             </a>
                             <a href="https://docs.google.com/document/d/1Uga481QOGNLg1TlUHGwxAzg9iQajox58en0XweD30_U/edit?usp=sharing" target="blank">
-                                <img alt="resume icon" src="/images/resume.png" /> <h3> Download a copy of my resume.</h3>
+                                <img alt="resume icon" src="/media/resume.png" /> <h3> Download a copy of my resume.</h3>
                             </a>
                         </div>
                     </div>
