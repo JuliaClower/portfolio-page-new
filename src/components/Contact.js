e240de30c7f5aa5100168f649b07ee26316e4b51
Julia Clower <juliaclower@Julias-MacBook-Air.local>
2019-12-11 14:49:00 -0500

    moved images to public directory

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,37 +1,33 @@
 import React, { Component } from 'react'
-// import emailIcon from "/Users/juliaclower/ga-sei/portfolio-page-new/src/circle-email-icon.png"
-// import githubIcon from "/Users/juliaclower/ga-sei/portfolio-page-new/src/github-icon.png"
-// import linkedinIcon from "/Users/juliaclower/ga-sei/portfolio-page-new/src/linkedin-icon.png"
-// import resumeIcon from "/Users/juliaclower/ga-sei/portfolio-page-new/src/resume.png"
+
 
 export default class Footer extends Component {
     render() {
-        return null
-        // return (
-        //     <div className="aboutPageMain">
-        //         <div className='say-hello-div'> <h1 className="hellowords">Say Hello, </h1></div>
-        //         <div className="outeraboutBox">
-        //             <div className="contact-icons">
-        //                 <h1></h1>
-        //                 <div className="contact-top-row">
-        //                     <a href="mailto:juliavanclower@gmail.com" target="_blank">
-        //                         <img alt="email icon" src={emailIcon} /> <h3>JuliaVanClower@gmail.com</h3>
-        //                     </a>
-        //                     <a href="https://github.com/JuliaClower" target="blank">
-        //                         <img alt="github icon" src={githubIcon} /> <h3>github.com/JuliaClower</h3>
-        //                     </a>
-        //                 </div>
-        //                 <div className="contact-bottom-row">
-        //                     <a href="https://www.linkedin.com/in/juliavanclower/" target="blank">
-        //                         <img alt="linkedin icon" src={linkedinIcon} /> <h3>linkedin.com/in/juliavanclower/ </h3>
-        //                     </a>
-        //                     <a href="https://docs.google.com/document/d/1Uga481QOGNLg1TlUHGwxAzg9iQajox58en0XweD30_U/edit?usp=sharing" target="blank">
-        //                         <img alt="resume icon" src={resumeIcon} /> <h3> Download a copy of my resume.</h3>
-        //                     </a>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // )
+        return (
+            <div className="aboutPageMain">
+                <div className='say-hello-div'> <h1 className="hellowords">Say Hello, </h1></div>
+                <div className="outeraboutBox">
+                    <div className="contact-icons">
+                        <h1></h1>
+                        <div className="contact-top-row">
+                            <a href="mailto:juliavanclower@gmail.com" target="_blank">
+                                <img alt="email icon" src="/images/circle-email-icon.png"/> <h3>JuliaVanClower@gmail.com</h3>
+                            </a>
+                            <a href="https://github.com/JuliaClower" target="blank">
+                                <img alt="github icon" src="/images/github-icon.png" /> <h3>github.com/JuliaClower</h3>
+                            </a>
+                        </div>
+                        <div className="contact-bottom-row">
+                            <a href="https://www.linkedin.com/in/juliavanclower/" target="blank">
+                                <img alt="linkedin icon" src="/images/linkedin-icon.png" /> <h3>linkedin.com/in/juliavanclower/ </h3>
+                            </a>
+                            <a href="https://docs.google.com/document/d/1Uga481QOGNLg1TlUHGwxAzg9iQajox58en0XweD30_U/edit?usp=sharing" target="blank">
+                                <img alt="resume icon" src="/images/resume.png" /> <h3> Download a copy of my resume.</h3>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
